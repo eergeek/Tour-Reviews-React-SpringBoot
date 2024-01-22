e66ef06bce187fc8502ca19668ad97f65d99d759
jambu <kadamjd99@gmail.com>
2024-01-22 20:24:34 +0530

    Init basic mvn project

---
+++ src/main/java/com/practice/demo/FullStackWithReactApplication.java
@@ -0,0 +1,13 @@
+package com.practice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackWithReactApplication.class, args);
+	}
+
+}
