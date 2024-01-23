89bb33ea80bbbad74cee0dbf6ba1c16d034453bb
jambu <kadamjd99@gmail.com>
2024-01-23 22:32:42 +0530

    @Lazy Bean Scope, won't create bean at startup

--- src/main/java/com/practice/demo/scopeofbeans/MySingletonComponent.java
+++ src/main/java/com/practice/demo/scopeofbeans/MySingletonComponent.java
@@ -1,8 +1,14 @@
 package com.practice.demo.scopeofbeans;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+/*
+ * Lazy Component Bean won't be created when SpringApplication.run(Application.class, args);
+ * It will be created when explicitly called context.getBean("mySingletonComponent");
+ */
+@Lazy
 public class MySingletonComponent {
     private static int nextId;
     private int id = nextId++;
